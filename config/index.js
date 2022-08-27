5f24a2c0c26d26d2c7b6f92edb314d5cf37e0aa4
TongRui乀 <wutr@dianzhong.com>
2022-08-27 09:34:45 +0800

    修改配置

--- config/index.js
+++ config/index.js
@@ -1,28 +1,28 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "",
+    appId: "wxd632422ef4dbfade",
     // 公众号appSecret
-    appSecret: "",
+    appSecret: "6101f98685ae898833d3c1c0ff561109",
     // 模板消息id
-    templateId: "",
+    templateId: "C2u487DsuqYweecyFSPl_rPx72HKAROXGsbHvJcUs_g",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
     user: [],
      
     // 信息配置
     // 所在省份
-    province: "广东",
+    province: "北京",
     // 所在城市
-    city: "惠州",
+    city: "海淀",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "老婆", "year": "1993", "date": "12-27", "type": 'new'},
-      {"name": "家公", "year": "1993", "date": "08-09", "type": 'new'},
+      {"name": "老婆", "year": "1999", "date": "12-12", "type": 'new'},
+      {"name": "家公", "year": "1994", "date": "08-15", "type": 'r'},
     ],
     // 在一起的日子，格式同上
-    loveDate: "2015-05-01",
+    loveDate: "2021-06-01",
     // 结婚纪念日
-    marryDate: "2020-01-04"
+    marryDate: "2022-10-03"
     }
 
 // {{date.DATA}}  
